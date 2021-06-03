0fba02f0fbd93b9983b42651dfab735da8178e1d
Dino <d.lin@tuta.io>
2021-06-03 13:02:31 +0200

    Create basic Java Spring project

---
+++ MS3/meilenstein3/src/main/java/de/thkoeln/meilenstein3/Meilenstein3Application.java
@@ -0,0 +1,13 @@
+package de.thkoeln.meilenstein3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Meilenstein3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Meilenstein3Application.class, args);
+	}
+
+}
---
+++ MS3/meilenstein3/src/test/java/de/thkoeln/meilenstein3/Meilenstein3ApplicationTests.java
@@ -0,0 +1,13 @@
+package de.thkoeln.meilenstein3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Meilenstein3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
